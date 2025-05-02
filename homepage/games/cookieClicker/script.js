888e46527a4692cd4036e05019c6dc014ef98caa
batman <talavy201@gmail.com>
2025-05-02 20:51:25 +0300

    showed text when your trying to use phones in games that doesnt support games

--- homepage/games/cookieClicker/script.js
+++ homepage/games/cookieClicker/script.js
@@ -1,122 +1,129 @@
 // localStorage.clear()
-let score = JSON.parse(localStorage.getItem('score')) || 0
-let scorePerClick = JSON.parse(localStorage.getItem('scorePerClick')) || 1
-let scorePerSec = JSON.parse(localStorage.getItem('scorePerSec')) || 0
-updateScore()
-
-let shopItems = [{
-    name: 'curser points +1',
-    price: 100,
-    lvl: 1,
-    action: 'curserPoints(',
-    spot: 0
-}, {
-    name: 'autoclicker +1',
-    price: 10,
-    lvl: 0,
-    action: 'autoclicker(',
-    spot: 1
-}, {
-    name: 'miner +5 points per/s',
-    price: 50,
-    lvl: 0,
-    action: 'minerPoints(',
-    spot: 2
-}, {
-    name: 'super miner +50 per/s',
-    price: 10000,
-    lvl: 0,
-    action: 'superMinerPoints(',
-    spot: 3
-}]
-
-for(i=0; i<shopItems.length;i++) {
-    const rawLvl = localStorage.getItem(`lvl${i}`);
-    if (rawLvl === 'undefined' || rawLvl == null) {
-        reloadShop()
-    } else {
-        shopItems[i].price = JSON.parse(localStorage.getItem(`price${i}`))
-        shopItems[i].lvl =  JSON.parse(localStorage.getItem(`lvl${i}`))
-    }
+if (window.innerWidth < 600) {
+    document.body.innerHTML = 'this game doesnt support phones only computers \n ill change this in the future'
 }
-
-document.querySelector('.cookie').addEventListener('click', (e) => {
-    score += scorePerClick
-    localStorage.setItem('score', JSON.stringify(score))
-    document.querySelector('.score').innerHTML = `score: ${score}`
-    floatingNumber = document.createElement('p')
-    floatingNumber.style.left = `${e.pageX}px`
-    floatingNumber.style.top = `${e.pageY}px`
-    floatingNumber.classList.add('floating-number')
-    floatingNumber.textContent = `+${scorePerClick}`
-    document.body.appendChild(floatingNumber)
-    setTimeout(() => {
-        document.querySelector('.floating-number').remove()
-    }, 1900)
-})
-
-function reloadShop() {
-    let ItemHtml = ''
-    shopItems.forEach((item) => {
-        item.price = Math.round(item.price);
-        ItemHtml += `<div class="item" onclick="${item.action}${item.spot})">
-                        <div class="item-name">${item.name}</div>
-                        <div class="item-price-lvl">${item.price}$  lvl: ${item.lvl}</div>
-                    </div>`
-    })
-    document.querySelector('.shop-body').innerHTML = ItemHtml
+else {
+    let score = JSON.parse(localStorage.getItem('score'))
+    if (score === undefined) {score = 0}
+    let scorePerClick = JSON.parse(localStorage.getItem('scorePerClick')) || 1
+    let scorePerSec = JSON.parse(localStorage.getItem('scorePerSec')) || 0
+    updateScore()
+    
+    let shopItems = [{
+        name: 'curser points +1',
+        price: 100,
+        lvl: 1,
+        action: 'curserPoints(',
+        spot: 0
+    }, {
+        name: 'autoclicker +1',
+        price: 10,
+        lvl: 0,
+        action: 'autoclicker(',
+        spot: 1
+    }, {
+        name: 'miner +5 points per/s',
+        price: 50,
+        lvl: 0,
+        action: 'minerPoints(',
+        spot: 2
+    }, {
+        name: 'super miner +50 per/s',
+        price: 10000,
+        lvl: 0,
+        action: 'superMinerPoints(',
+        spot: 3
+    }]
+    
     for(i=0; i<shopItems.length;i++) {
-        localStorage.setItem(`price${i}`, JSON.stringify(shopItems[i].price))
-        localStorage.setItem(`lvl${i}`, JSON.stringify(shopItems[i].lvl))
+        const rawLvl = localStorage.getItem(`lvl${i}`);
+        if (rawLvl === 'undefined' || rawLvl == null) {
+            reloadShop()
+        } else {
+            shopItems[i].price = JSON.parse(localStorage.getItem(`price${i}`))
+            shopItems[i].lvl =  JSON.parse(localStorage.getItem(`lvl${i}`))
+        }
     }
-    localStorage.setItem('scorePerSec', JSON.stringify(scorePerSec))
-    localStorage.setItem('scorePerClick', JSON.stringify(scorePerClick))
-}
-reloadShop()
-
-
-function autoclicker(spot) {
-    if (score - shopItems[spot].price >= 0) {
-        scorePerSec += 1
-        reloadItem(spot, 1.1)
+    
+    document.querySelector('.cookie').addEventListener('click', (e) => {
+        score += scorePerClick
+        localStorage.setItem('score', JSON.stringify(score))
+        document.querySelector('.score').innerHTML = `score: ${score}`
+        floatingNumber = document.createElement('p')
+        floatingNumber.style.left = `${e.pageX}px`
+        floatingNumber.style.top = `${e.pageY}px`
+        floatingNumber.classList.add('floating-number')
+        floatingNumber.textContent = `+${scorePerClick}`
+        document.body.appendChild(floatingNumber)
+        setTimeout(() => {
+            document.querySelector('.floating-number').remove()
+        }, 1900)
+    })
+    
+    function reloadShop() {
+        let ItemHtml = ''
+        shopItems.forEach((item) => {
+            item.price = Math.round(item.price);
+            ItemHtml += `<div class="item" onclick="${item.action}${item.spot})">
+                            <div class="item-name">${item.name}</div>
+                            <div class="item-price-lvl">${item.price}$  lvl: ${item.lvl}</div>
+                        </div>`
+        })
+        document.querySelector('.shop-body').innerHTML = ItemHtml
+        for(i=0; i<shopItems.length;i++) {
+            localStorage.setItem(`price${i}`, JSON.stringify(shopItems[i].price))
+            localStorage.setItem(`lvl${i}`, JSON.stringify(shopItems[i].lvl))
+        }
+        localStorage.setItem('scorePerSec', JSON.stringify(scorePerSec))
+        localStorage.setItem('scorePerClick', JSON.stringify(scorePerClick))
     }
-}
-
-function minerPoints(spot) {
-    if (score - shopItems[spot].price >= 0) {
-        scorePerSec += 5
-        reloadItem(spot, 1.2)
-    }
-}
-function curserPoints(spot) {
-    if (score - shopItems[spot].price >= 0) {
-        scorePerClick += 1
-        reloadItem(spot, 1.2)
-    }
-}
-function superMinerPoints(spot) {
-    if (score - shopItems[spot].price >= 0) {
-        scorePerSec += 50
-        reloadItem(spot, 1.3)
-    }
-}
-
-function updateScore() {
-    score = Math.round(score);
-    localStorage.setItem('score', JSON.stringify(score))
-    document.querySelector('.score').innerHTML = `score: ${score}`
-    document.querySelector('.score-per-sec').innerHTML = `score per/s: ${scorePerSec}`
-}
-
-function reloadItem(x, num) {
-    score -= shopItems[x].price
-    updateScore()
-    shopItems[x].price *= num
-    shopItems[x].lvl += 1
     reloadShop()
+    
+    
+    function autoclicker(spot) {
+        if (score - shopItems[spot].price >= 0) {
+            scorePerSec += 1
+            reloadItem(spot, 1.1)
+        }
+    }
+    
+    function minerPoints(spot) {
+        if (score - shopItems[spot].price >= 0) {
+            scorePerSec += 5
+            reloadItem(spot, 1.2)
+        }
+    }
+    function curserPoints(spot) {
+        if (score - shopItems[spot].price >= 0) {
+            scorePerClick += 1
+            reloadItem(spot, 1.2)
+        }
+    }
+    function superMinerPoints(spot) {
+        if (score - shopItems[spot].price >= 0) {
+            scorePerSec += 50
+            reloadItem(spot, 1.3)
+        }
+    }
+    
+    function updateScore() {
+        score = Math.round(score);
+        localStorage.setItem('score', JSON.stringify(score))
+        document.querySelector('.score').innerHTML = `score: ${score}`
+        document.querySelector('.score-per-sec').innerHTML = `score per/s: ${scorePerSec}`
+    }
+    
+    function reloadItem(x, num) {
+        score -= shopItems[x].price
+        updateScore()
+        shopItems[x].price *= num
+        shopItems[x].lvl += 1
+        reloadShop()
+    }
+    
+    setInterval(() => {
+        score += scorePerSec 
+        updateScore()
+    }, 1000) 
+    
 }
-
-setInterval(() => {
-    score += scorePerSec 
-    updateScore()
-}, 1000) 
